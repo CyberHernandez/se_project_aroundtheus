07e192d7a2f8af14ae55e3e80ec1b97f9342db60
VHernandez <vivianazhernandez@gmail.com>
2023-03-29 10:19:07 -0700

    Project 4 Stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,15 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+/*  ------ELEMENTS-------*/
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const modalCloseButton = document.querySelector("#modal-close-button");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+modalCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
