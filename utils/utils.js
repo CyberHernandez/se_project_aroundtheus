e709738626ef279413dced0acabd0565d5fd1e49
VHernandez <vivianazhernandez@gmail.com>
2023-05-25 16:02:52 -0700

    util corrections

--- utils/utils.js
+++ utils/utils.js
@@ -1,30 +1,28 @@
-function handleEscape(e) {
-  console.log(e);
-  if (e.key === "Escape") {
-    const modal = document.querySelector(".modal_opened");
-    console.log(modal);
-    closeModal(modal);
-  }
-}
-function handleOverlayClick(evt) {
-  const openedModal = document.querySelector(".modal_opened");
-  if (
-    evt.target.classList.contains("modal") ||
-    evt.target.classList.contains("modal__close-button")
-  ) {
-    closeModal(openedModal);
-  }
-}
-
 export function openModal(modal) {
   modal.classList.add("modal_opened");
-  console.log(modal);
-  modal.addEventListener("keyup", handleEscape);
-  modal.addEventListener("mousedown", handleOverlayClick);
+  document.addEventListener("keydown", handleEscape);
+  document.addEventListener("click", handleOverlayClick);
 }
 
 export function closeModal(modal) {
   modal.classList.remove("modal_opened");
-  modal.removeEventListener("keyup", handleEscape);
-  modal.removeEventListener("mousedown", handleOverlayClick);
+  document.removeEventListener("keydown", handleEscape);
+  document.removeEventListener("click", handleOverlayClick);
+}
+
+function handleEscape(e) {
+  if (e.key === "Escape") {
+    const modal = document.querySelector(".modal_opened");
+    closeModal(modal);
+  }
+}
+
+function handleOverlayClick(evt) {
+  if (
+    evt.target.classList.contains("modal") ||
+    evt.target.classList.contains("modal__close-button")
+  ) {
+    const openedModal = document.querySelector(".modal_opened");
+    closeModal(openedModal);
+  }
 }
